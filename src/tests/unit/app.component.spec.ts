e457db7fcfe888d785205698dc48a7e76d0feba0
Juanmi González <juanmiguel@bdeo.io>
2023-01-26 16:43:12 +0100

    chore: add environments and tests folders

--- src/tests/unit/app.component.spec.ts
+++ src/tests/unit/app.component.spec.ts
@@ -1,16 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent } from './app.component';
+import { AppComponent } from '../../app/app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterTestingModule],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
@@ -29,7 +25,9 @@ describe('AppComponent', () => {
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-clean-architecture app is running!');
+    const compiled = fixture.nativeElement satisfies HTMLElement;
+    expect(compiled.querySelector('.content span')?.textContent).toContain(
+      'angular-clean-architecture app is running!'
+    );
   });
 });
