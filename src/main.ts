1dd36380dc8a3a4677aedf4d5da4dcbd878dd88d
Juanmi González <jmgonzalezit@gmail.com>
2023-06-09 20:10:45 +0200

    chore: migrate angular project to standalone

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,16 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
 
-platformBrowserDynamic()
-  .bootstrapModule(AppModule)
+import { importProvidersFrom } from '@angular/core';
+import { AppComponent } from './app/app.component';
+import { withInterceptorsFromDi, provideHttpClient } from '@angular/common/http';
+import { AppRoutingModule } from './app/app-routing.module';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+
+bootstrapApplication(AppComponent, {
+    providers: [
+        importProvidersFrom(BrowserModule, AppRoutingModule),
+        provideHttpClient(withInterceptorsFromDi())
+    ]
+})
   .catch((err) => console.error(err));
